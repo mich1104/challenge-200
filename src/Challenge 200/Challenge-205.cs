1860971e85a432957cfde7515e556ade8b650a8b
Michael Overgaard <m.g.overgaard@hotmail.com>
2021-01-27 13:10:28 +0100

    challenge-205: encoding complete

--- src/Challenge 200/Challenge-205.cs
+++ src/Challenge 200/Challenge-205.cs
@@ -4,9 +4,28 @@ namespace Challenge_200
 {
     public class Challenge_205
     {
+        /// <summary>
+        /// Encodes the <paramref name="messageToEncode"/>.
+        /// </summary>
+        /// <param name="messageToEncode">The message to encode</param>
+        /// <returns>The encoded message</returns>
         public int[] Encode(string messageToEncode)
         {
-            throw new NotImplementedException();
+            int[] encoded = new int[messageToEncode.Length];
+
+            for (int i = 0; i < messageToEncode.Length; i++) // encoding is done char by char
+            {
+                if (i == 0) // first char just gets it's numeric value
+                {
+                    encoded[i] = (int)messageToEncode[i];
+                    continue;
+                }
+
+                // all subsequent chars gets a value based on the difference in numeric value to the former char
+                encoded[i] = messageToEncode[i] - messageToEncode[i - 1];
+            }
+
+            return encoded;
         }
         public string Decode(int[] encodedMessage)
         {
